49f1a461aec307a99fb83fb9f94929d5978cf464
Ruslan <dezign.ruslan@gmail.com>
2022-09-05 21:50:52 +0300

    Папка с JS кодом, развивается вместе с Русланом. Или нет

---
+++ alert.js
@@ -0,0 +1,11 @@
+function pow(x,n) {
+    let result = x**n
+    return result;
+}
+
+x = +prompt ('Какое число?', )
+n = +prompt ('Какая степень?', )
+
+alert (pow (x, n))
+
+// https://learn.javascript.ru/function-basics
